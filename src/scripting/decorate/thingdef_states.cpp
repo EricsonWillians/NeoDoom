5dc39b232d3d742a66d30e0073d18a5360043e4f
Ericson Willians <ericsonwillians@protonmail.com>
2026-07-07 09:07:09 -0300

    Restore DECORATE action return compatibility

--- src/scripting/decorate/thingdef_states.cpp
+++ src/scripting/decorate/thingdef_states.cpp
@@ -475,7 +475,7 @@ FxExpression *ParseActions(FScanner &sc, FState state, FString statestring, Bagg
 	{
 		FxExpression *call = ParseAction(sc, state, statestring, bag);
 		endswithret = true;
-		return call;
+		return new FxReturnStatement(call, sc);
 	}
 
 	const FScriptPosition pos(sc);

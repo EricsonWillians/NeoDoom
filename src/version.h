db5b00eb454f96c798326806ede88849b8ad1523
Ericson Willians <ericsonwillians@protonmail.com>
2026-08-14 12:42:21 -0300

    Bump BiasedDoom version to 4.15.10

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.15.9"
+#define VERSIONSTR "4.15.10"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,15,9,0
-#define RC_PRODUCTVERSION 4,15,9,0
+#define RC_FILEVERSION 4,15,10,0
+#define RC_PRODUCTVERSION 4,15,10,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
